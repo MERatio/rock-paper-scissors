beabec0851e6be63b660b877ff6621310649c68b
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-07-30 07:53:59 +0800

    Create getComputerChoice()

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log(1);
+function getRandomElement(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  return getRandomElement(choices);
+}
