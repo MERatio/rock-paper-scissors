7e5c43ed2a624f911da981c759c15213974de91d
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-07-30 09:10:48 +0800

    Create playGame()

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 'use strict';
 
-let humanScore = 0;
-let computerScore = 0;
-
 function getRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function capitalizeFirstLetter(str) {
   return str[0].toUpperCase() + str.slice(1);
+}
+
+function generateWinnerAnnouncement(humanScore, computerScore) {
+  let logText = '';
+  if (humanScore === computerScore) {
+    logText += `It's a tie game.`;
+  } else if (humanScore > computerScore) {
+    logText += `You win the game!.`;
+  } else {
+    logText += `You lose the game!.`;
+  }
+  logText += ` Human: ${humanScore}, Computer: ${computerScore}`;
+  return logText;
 }
 
 function getComputerChoice() {
@@ -21,33 +31,48 @@ function getHumanChoice() {
   return choice.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-  if (humanChoice === computerChoice) {
-    console.log(
-      `It's a tie! Both picked ${capitalizeFirstLetter(computerChoice)}`
-    );
-  } else if (
-    (humanChoice === 'rock' && computerChoice === 'scissors') ||
-    (humanChoice === 'paper' && computerChoice === 'rock') ||
-    (humanChoice === 'scissors' && computerChoice === 'paper')
-  ) {
-    humanScore++;
-    console.log(
-      `You win! ${capitalizeFirstLetter(
-        humanChoice
-      )} beats ${capitalizeFirstLetter(computerChoice)}`
-    );
-  } else {
-    computerScore++;
-    console.log(
-      `You lose! ${capitalizeFirstLetter(
-        computerChoice
-      )} beats ${capitalizeFirstLetter(humanChoice)}`
-    );
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      console.log(
+        `It's a tie! Both picked ${capitalizeFirstLetter(computerChoice)}`
+      );
+    } else if (
+      (humanChoice === 'rock' && computerChoice === 'scissors') ||
+      (humanChoice === 'paper' && computerChoice === 'rock') ||
+      (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+      humanScore++;
+      console.log(
+        `You win! ${capitalizeFirstLetter(
+          humanChoice
+        )} beats ${capitalizeFirstLetter(computerChoice)}`
+      );
+    } else {
+      computerScore++;
+      console.log(
+        `You lose! ${capitalizeFirstLetter(
+          computerChoice
+        )} beats ${capitalizeFirstLetter(humanChoice)}`
+      );
+    }
   }
+
+  // Play 5 rounds.
+  for (let i = 0; i < 5; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
+
+  const winnerAnnouncement = generateWinnerAnnouncement(
+    humanScore,
+    computerScore
+  );
+  console.log(winnerAnnouncement);
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
-playRound(humanChoice, computerChoice);
+playGame();
