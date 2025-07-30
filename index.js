ce8cff136f60b95ba9acb7ff3070ba4af5204443
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-07-30 08:45:14 +0800

    Create playRound()

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 function getRandomElement(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function capitalizeFirstLetter(str) {
+  return str[0].toUpperCase() + str.slice(1);
 }
 
 function getComputerChoice() {
@@ -14,5 +18,36 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   const choice = prompt('rock, paper, or scissors?');
-  return choice;
+  return choice.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log(
+      `It's a tie! Both picked ${capitalizeFirstLetter(computerChoice)}`
+    );
+  } else if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    humanScore++;
+    console.log(
+      `You win! ${capitalizeFirstLetter(
+        humanChoice
+      )} beats ${capitalizeFirstLetter(computerChoice)}`
+    );
+  } else {
+    computerScore++;
+    console.log(
+      `You lose! ${capitalizeFirstLetter(
+        computerChoice
+      )} beats ${capitalizeFirstLetter(humanChoice)}`
+    );
+  }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
