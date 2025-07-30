80a425cd776d182ecd48c9991e91646dd9b0383a
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2025-07-30 08:00:31 +0800

    Create getHumanChoice()

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function getComputerChoice() {
   const choices = ['rock', 'paper', 'scissors'];
   return getRandomElement(choices);
 }
+
+function getHumanChoice() {
+  const choice = prompt('rock, paper, or scissors?');
+  return choice;
+}
